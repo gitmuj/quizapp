e0dbf98903823c55f348fc8be991ba9102152c7e
Muj <muj.mohmand@gmail.com>
2020-06-18 18:30:54 -0400

    Updates

---
+++ precache-manifest.bc00a9b259f7198261cf90eb582ee38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceeadf29ee1199984fc9ebbb3a767fb3",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "cd2a1f4fbfaff3b78113",
+    "url": "/quizapp/static/css/main.78e8db8c.chunk.css"
+  },
+  {
+    "revision": "7584bf27948fdce6be9f",
+    "url": "/quizapp/static/js/2.67f9104f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/quizapp/static/js/2.67f9104f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2a1f4fbfaff3b78113",
+    "url": "/quizapp/static/js/main.fbb8b25d.chunk.js"
+  },
+  {
+    "revision": "b439d77e90ffa5b97075",
+    "url": "/quizapp/static/js/runtime-main.fa77bbea.js"
+  }
+]);
